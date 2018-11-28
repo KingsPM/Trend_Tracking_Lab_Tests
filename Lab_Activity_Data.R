a34203b609d83c8acbd96556d9b6fc91130fb38a
Callum-Rakhit <callum_rakhit@hotmail.com>
2018-11-28 14:58:16 +0000

    created a main() function

--- Lab_Activity_Data.R
+++ Lab_Activity_Data.R
@@ -1,8 +1,7 @@
 #!/usr/bin/env Rscript
 
-# TODO 
-# - Specify input and output on the cmd line
-# - Wrap everything in functions
+# TODO(Callum):
+#   Strip out as many libraries as possible
 
 library(tcltk)
 
@@ -14,19 +13,19 @@ inputexcel <- tk_choose.files(default = '',
 outdir <- tk_choose.dir(default = getwd(), 
                             caption = "Where should the output be saved?")
 
-# test if there is at least one argument: if not, return an error
+if (length(inputexcel) == 0) {
+  stop("You need to add an input Excel file and a output location", call. = FALSE)
+}
 
-# if (length(args) == 0) {
-#   stop("You need to add an input Excel file and a output location", call. = FALSE)
-# }
+# test if there is at least one argument: if not, return an error
 
 ##### Install packages and load libraries #####
 
 # Install packages
 
-install.packages(c("readxl", "tidyverse", "ggplot2", "reshape2", "lubridate",
-                   "xts", "data.table", "gridExtra", "rJava", "xlsxjars",
-                   "xlsx", "openxlsx", "dplyr"))
+# install.packages(c("readxl", "tidyverse", "ggplot2", "reshape2", "lubridate",
+#                    "xts", "data.table", "gridExtra", "rJava", "xlsxjars",
+#                    "xlsx", "openxlsx", "dplyr"))
 
 # (tidyverse loads ggplot2, dplyr, tidyr, readr, purrr, tibble, stringr, forcats)
 
@@ -46,92 +45,103 @@ data <- read_excel(inputexcel)  # Get excel from first argument
 
 # Produce a table (Audit) that has the turnaround times for each investigation
 
-Audit <- data[c("SIDC09 Hospital", "SIDC13 Investigation Requested", "SIDC16 Date Sample Received", "Reported date", "Real Billing category")]
-colnames(Audit) <- c("Hospital", "Investigation", "Sample_Received", "Test_Reported", "Billing_Category")
-Audit$Test_Reported <- as.Date(Audit$Test_Reported)
-Audit$Sample_Received <- as.Date(Audit$Sample_Received)
-Audit$Turnover_Time <- Audit$Test_Reported - Audit$Sample_Received
-Audit$Turnover_Time <- as.numeric(Audit$Turnover_Time)
-Audit$Year_Reported <- format(as.Date(Audit$Test_Reported, format="%d/%m/%Y"),"%Y")
-Audit$Month_Reported <- format(as.Date(Audit$Test_Reported, format="%d/%m/%Y"),"%m")
-Audit$Day_Reported <- format(as.Date(Audit$Test_Reported, format="%d/%m/%Y"),"%d")
-Audit[9] <- paste("Y", Audit$Year_Reported, "_M", Audit$Month_Reported, sep="")
-colnames(Audit)[9] <- "Year_Month"
-Audit[10] <- (30)
-colnames(Audit)[10] <- "Target_Turnaround"
+# examplefunction <- function(a) {
+#   print(a)
+# }
+# 
+# examplefunction('this')
 
-auditNoNAs <- na.omit(Audit)
+
+createDF <- function(data) {
+  Audit <- data[c("SIDC09 Hospital", "SIDC13 Investigation Requested", "SIDC16 Date Sample Received", "Reported date", "Real Billing category")]
+  colnames(Audit) <- c("Hospital", "Investigation", "Sample_Received", "Test_Reported", "Billing_Category")
+  Audit$Test_Reported <- as.Date(Audit$Test_Reported)
+  Audit$Sample_Received <- as.Date(Audit$Sample_Received)
+  Audit$Turnover_Time <- Audit$Test_Reported - Audit$Sample_Received
+  Audit$Turnover_Time <- as.numeric(Audit$Turnover_Time)
+  Audit$Year_Reported <- format(as.Date(Audit$Test_Reported, format="%d/%m/%Y"),"%Y")
+  Audit$Month_Reported <- format(as.Date(Audit$Test_Reported, format="%d/%m/%Y"),"%m")
+  Audit$Day_Reported <- format(as.Date(Audit$Test_Reported, format="%d/%m/%Y"),"%d")
+  Audit[9] <- paste("Y", Audit$Year_Reported, "_M", Audit$Month_Reported, sep="")
+  colnames(Audit)[9] <- "Year_Month"
+  Audit[10] <- (30)
+  colnames(Audit)[10] <- "Target_Turnaround"
+  Audit <- na.omit(Audit)
+  return(Audit)
+}
 
 # If you wanted to only count weekdays -> don't want to do this currently
 # sum(!grepl("S", weekdays(seq(Sys.Date(), as.Date(scan(,""), "%d.%m.%Y"), 1)))) + 1
 
 ##### Plot turnover time of each test per month #####
 
-turnoverTime <- auditNoNAs[c("Investigation", "Year_Month", "Turnover_Time", "Target_Turnaround")]
-turnoverTime <- na.omit(turnoverTime)
 
-pdf(paste0(outdir, "output/Turnover Time Per Test.pdf"))
-sort(unique(turnoverTime$Investigation))
-lapply(sort(unique(turnoverTime$Investigation)), 
-       function(i) {ggplot(turnoverTime[turnoverTime$Investigation == i,],
-  aes(x = Year_Month, y = Turnover_Time)) +
-  labs(title = i,
-    subtitle = "Outliers, defined as ±1.5*IQR, have not been plotted. Boxplots \nrepresent Median, Q1-Q3 and 1.5*Q1Q3. Red line is the mean.",
-    y = "Turnaround Time (Days)",
-    x = "Year and Month") +
-  geom_boxplot(alpha = 0.80, outlier.colour = "black", 
-  outlier.shape = NA, outlier.size = .5, notch = FALSE,
-  fill = "lightskyblue2", color = "black") +
-  stat_summary(aes(y = Turnover_Time, group = 1), fun.y = mean, 
-  colour="red", geom="line", group = 1) +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1),
-  panel.grid.major.x = element_blank(), 
-  panel.grid.major.y = element_line(size = .1, color = "black")) +
-  scale_x_discrete(limits = sort(unique(auditNoNAs$Year_Month)), breaks = sort(unique(auditNoNAs$Year_Month))) +    
-  ylim(-5, quantile(turnoverTime[turnoverTime$Investigation == i,][[3]])[[4]]+10) +
-  geom_hline(yintercept = auditNoNAs$Target_Turnaround, linetype = "dashed", 
-    color = "red", size = .5)
-})
-
-dev.off()
+createPDF1 <- function(data) {
+  turnoverTime <- data[c("Investigation", "Year_Month", "Turnover_Time", "Target_Turnaround")]
+  turnoverTime <- na.omit(turnoverTime)
+  pdf(paste0(outdir, "/output/Turnover Time Per Test.pdf"))
+  sort(unique(turnoverTime$Investigation))
+  lapply(sort(unique(turnoverTime$Investigation)), 
+         function(i) {ggplot(turnoverTime[turnoverTime$Investigation == i,],
+                             aes(x = Year_Month, y = Turnover_Time)) +
+             labs(title = i, subtitle = "Outliers, defined as ±1.5*IQR, output
+                  have not been plotted. Boxplots \nrepresent Median, Q1-Q3 
+                  and 1.5*Q1Q3. Red line is the mean.",
+                  y = "Turnaround Time (Days)",
+                  x = "Year and Month") +
+             geom_boxplot(alpha = 0.80, outlier.colour = "black", 
+                          outlier.shape = NA, outlier.size = .5, notch = FALSE,
+                          fill = "lightskyblue2", color = "black") +
+             stat_summary(aes(y = Turnover_Time, group = 1), fun.y = mean,
+                          colour="red", geom="line", group = 1) +
+             theme_minimal() + theme(axis.text.x = element_text(angle = 45, hjust = 1),
+                                     panel.grid.major.x = element_blank(), 
+                                     panel.grid.major.y = element_line(size = .1, color = "black")) +
+             scale_x_discrete(limits = sort(unique(data$Year_Month)),
+                              breaks = sort(unique(data$Year_Month))) +
+             ylim(-5, quantile(turnoverTime[turnoverTime$Investigation == i,][[3]])[[4]]+10) +
+             geom_hline(yintercept = data$Target_Turnaround, linetype = "dashed",
+                        color = "red", size = .5)}
+         )
+  dev.off()
+}
 
 ##### Print out some excel sheets #####
 
 # Create dataset
 
-Hospital <- Audit[c("Hospital", "Investigation", "Year_Month")]
-
 # Save test frequency and referring hospital for each year 
 
-wb = createWorkbook(paste0(outdir, "output/Investigations per hospital per month"))
 
-for (i in sort(unique(Hospital$Year_Month))){
-  excelPrintout <- Hospital[Hospital$Year_Month == i,]
-  excelPrintout <- excelPrintout[c("Hospital", "Investigation")]
-  excelPrintout <- as.data.frame(table(excelPrintout))
-  row_sub <- excelPrintout$Freq != 0
-  excelPrintout <- excelPrintout[row_sub,]
-  addWorksheet(wb = wb, 
-    sheetName = i) # Doesn't print?
-  writeData(wb = wb,
-    sheet = i, x = excelPrintout)
-  saveWorkbook(wb, paste0(outdir, "output/Investigations per hospital per month.xlsx"), overwrite = TRUE)
+createExcel1 <- function(data) {
+  wb = createWorkbook(paste0(outdir, "/output/Investigations per hospital per month"))
+  Hospital <- data[c("Hospital", "Investigation", "Year_Month")]
+  for (i in sort(unique(Hospital$Year_Month))){
+    excelPrintout <- Hospital[Hospital$Year_Month == i,]
+    excelPrintout <- excelPrintout[c("Hospital", "Investigation")]
+    excelPrintout <- as.data.frame(table(excelPrintout))
+    row_sub <- excelPrintout$Freq != 0
+    excelPrintout <- excelPrintout[row_sub,]
+    addWorksheet(wb = wb, sheetName = i)  # Doesn't print?
+    writeData(wb = wb, sheet = i, x = excelPrintout)
+    saveWorkbook(wb, paste0(outdir, "/output/Investigations per hospital per month.xlsx"),
+               overwrite = TRUE)}
 }
 
-wb1 = createWorkbook(paste0(outdir, "output/Investigations Each Month By Hospital"))
 
-for (i in sort(unique(Hospital$Investigation))){
-  excelPrintout <- Hospital[Hospital$Investigation == i,]
-  excelPrintout <- excelPrintout[c("Hospital", "Year_Month")]
-  excelPrintout <- as.data.frame(table(excelPrintout))
-  row_sub <- excelPrintout$Freq != 0
-  excelPrintout <- excelPrintout[row_sub,]
-  addWorksheet(wb = wb1, 
-    sheetName = substr(i, 1, 31))  # Needed to substring as name limit of 31 characters
-  writeData(wb = wb1,
-    sheet = substr(i, 1, 31), x = excelPrintout)
-  saveWorkbook(wb1, paste0(outdir, "output/Investigations per month per hospital.xlsx"), overwrite = TRUE)
+createExcel2 <- function(data) {
+  wb = createWorkbook(paste0(outdir, "/output/Investigations Each Month By Hospital"))
+  Hospital <- data[c("Hospital", "Investigation", "Year_Month")]
+  for (i in sort(unique(Hospital$Investigation))){
+    excelPrintout <- Hospital[Hospital$Investigation == i,]
+    excelPrintout <- excelPrintout[c("Hospital", "Year_Month")]
+    excelPrintout <- as.data.frame(table(excelPrintout))
+    row_sub <- excelPrintout$Freq != 0
+    excelPrintout <- excelPrintout[row_sub,]
+    addWorksheet(wb = wb, sheetName = substr(i, 1, 31))  # Needed to substring as name limit of 31 characters
+    writeData(wb = wb, sheet = substr(i, 1, 31), x = excelPrintout)
+    saveWorkbook(wb, paste0(outdir, "/output/Investigations per month per hospital.xlsx"),
+                 overwrite = TRUE)}
 }
 
 ###############################################################################
@@ -141,40 +151,39 @@ for (i in sort(unique(Hospital$Investigation))){
 # Count the number of results for each investigation. Convert investigations into 
 # a frequency while retaining year and month as a subcategory.
 
-Investigation <- Audit[c("Investigation", "Year_Reported", "Month_Reported")]
-Investigation <- Investigation[!(Investigation$Year_Reported == "1899"),]  # rm anomalous data
-Investigation <- as.data.frame(table(Investigation))
-Investigation[Investigation == 0] <- NA
 
-# Plot line for number of investigations done each month, year on year
-
-pdf(paste0(outdir, "output/Tests Each Month.pdf"))
-
-lapply(sort(unique(Investigation$Investigation)), 
+createPDF2 <- function(data) {
+  Investigation <- data[c("Investigation", "Year_Reported", "Month_Reported")]
+  Investigation <- Investigation[!(Investigation$Year_Reported == "1899"),]  # rm anomalous data
+  Investigation <- as.data.frame(table(Investigation))
+  Investigation[Investigation == 0] <- NA
+  
+  # Plot line for number of investigations done each month, year on year
+  
+  pdf(paste0(outdir, "/output/Tests Each Month.pdf"))
+  
+  lapply(sort(unique(Investigation$Investigation)), 
     function(i) {ggplot(Investigation[Investigation$Investigation == i,], 
       aes(Month_Reported, Freq, group = Year_Reported, color = Year_Reported)) +
-    labs(title = i,
-      y = "Frequency",
-      x = "Month") +
-    geom_point() +
-    geom_line() +
-    theme_minimal() +
-    theme(axis.text.x = element_text() ,# , hjust = 1),
-      panel.grid.major.x = element_blank(),
-      panel.grid.major.y = element_line(size = .1, color = "black"))
-})
+    labs(title = i, y = "Frequency", x = "Month") + geom_point() +
+    geom_line() + theme_minimal() + theme(axis.text.x = element_text(),
+                                          panel.grid.major.x = element_blank(),
+                                          panel.grid.major.y = element_line(size = .1, color = "black"))
+      })
 
 dev.off()
 
+}
+
 ##### Number of billing types per month, year on year #####
 
-workFreq <- auditNoNAs[c("Billing_Category", "Year_Reported", "Month_Reported")]
-workFreq <- as.data.frame(table(workFreq))
-workFreq[workFreq == 0] <- NA
 
-pdf(paste0(outdir, "output/Billing Categories.pdf"))
-
-lapply(sort(unique(workFreq$Billing_Category)), 
+createPDF3 <- function(data) {
+  workFreq <- data[c("Billing_Category", "Year_Reported", "Month_Reported")]
+  workFreq <- as.data.frame(table(workFreq))
+  workFreq[workFreq == 0] <- NA
+  pdf(paste0(outdir, "/output/Billing Categories.pdf"))
+  lapply(sort(unique(workFreq$Billing_Category)), 
   function(i) {ggplot(workFreq[workFreq$Billing_Category == i,], 
     aes(Month_Reported, Freq, group = Year_Reported, color = Year_Reported)) +
     labs(title = i,
@@ -186,33 +195,50 @@ lapply(sort(unique(workFreq$Billing_Category)),
     theme(axis.text.x = element_text(angle = 45, hjust = 1),
       panel.grid.major.x = element_blank(), 
       panel.grid.major.y = element_line(size = .1, color = "black"))
+    })
+
+dev.off()
+
+}
+
+
+createPDF4 <- function(data) {
+  topHospitals <- data[c("Hospital")]
+  topHospitals <- as.data.frame(table(topHospitals))
+  topHospitals <- topHospitals[ave(-topHospitals$Freq, FUN = rank) <= 10, ]
+  referrerHospital <- data[c("Hospital", "Year_Reported", "Month_Reported")]
+  referrerHospital <- as.data.frame(table(referrerHospital))
+  referrerHospital <- subset(referrerHospital, Hospital %in% topHospitals$topHospitals)
+  referrerHospital[referrerHospital == 0] <- NA
+  pdf(paste0(outdir, "/output/Top 10 Referrers.pdf"))
+  lapply(sort(unique(referrerHospital$Hospital)),
+         function(i) {ggplot(referrerHospital[referrerHospital$Hospital == i,],
+                             aes(Month_Reported, Freq, group = Year_Reported, color = Year_Reported)) +
+             labs(title = i, y = "Frequency", x = "Month") + geom_point() +
+             geom_line() + theme_minimal() + theme(axis.text.x = element_text(angle = 45, hjust = 1),
+                                                   panel.grid.major.x = element_blank(),
+                                                   panel.grid.major.y = element_line(size = .1, color = "black"))
 })
 
 dev.off()
 
-topHospitals <- auditNoNAs[c("Hospital")]
-topHospitals <- as.data.frame(table(topHospitals))
-topHospitals <- topHospitals[ave(-topHospitals$Freq, FUN = rank) <= 10, ]
+}
 
-referrerHospital <- auditNoNAs[c("Hospital", "Year_Reported", "Month_Reported")]
-referrerHospital <- as.data.frame(table(referrerHospital))
-referrerHospital <- subset(referrerHospital, Hospital %in% topHospitals$topHospitals)
-referrerHospital[referrerHospital == 0] <- NA
+# Audit <- createDF(data)
 
-pdf(paste0(outdir, "output/Top 10 Referrers.pdf"))
 
-lapply(sort(unique(referrerHospital$Hospital)), 
-  function(i) {ggplot(referrerHospital[referrerHospital$Hospital == i,], 
-    aes(Month_Reported, Freq, group = Year_Reported, color = Year_Reported)) +
-    labs(title = i,
-      y = "Frequency",
-      x = "Month") +
-    geom_point() +
-    geom_line() +
-    theme_minimal() +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1),
-      panel.grid.major.x = element_blank(), 
-      panel.grid.major.y = element_line(size = .1, color = "black"))
-})
+main <- function(input) {
+  createPDF1(input)
+  createPDF2(input)
+  createPDF3(input)
+  createPDF4(input)
+  createExcel1(input)
+  createExcel2(input)
+}
 
-dev.off()
+
+if (!interactive()) {
+  main(createDF(data))
+}
+
+main(createDF(data))
