a197ab3f999debdb04053c91f849ecf8ec380903
Callum-Rakhit <callum_rakhit@hotmail.com>
2018-12-17 17:35:15 +0000

    added drop downs that allow select all/select multiple

--- shinyapp.R
+++ shinyapp.R
@@ -18,7 +18,7 @@ getPackages <- function(required.packages) {
 
 
 getPackages(c("tcltk", "reshape2", "lubridate", "xts", "data.table",
-              "ggplot2", "gridExtra", "openxlsx", "shiny"))
+              "ggplot2", "gridExtra", "openxlsx", "shiny", "shinyWidgets"))
 
 # input.csv <- tk_choose.files(default = '',
 #                                caption = "Please select the input excel file")
@@ -67,12 +67,12 @@ CreateDF <- function(data) {
   # Create a "Year.Month" column to label the x-axis with
   
   data.cleaned$Year.Month <- paste("Year ", data.cleaned$Year.Reported, ", Month ", data.cleaned$Month.Reported, sep="")
-  data.cleaned$Target.Turnaround <- (30)
+  data.cleaned$Target.Turnaround <- (10)
   data.cleaned <- na.omit(data.cleaned)
-  
   return(data.cleaned)
   
 }
+
 
 data <- read.csv(input.excel)  # Get data frame from input excel file
 data.cleaned <- CreateDF(data)
@@ -81,6 +81,7 @@ data.cleaned <- CreateDF(data)
 TurnoverPlot <- function(test, date) {
   plot.data <- data.cleaned[data.cleaned$Investigation == test,]
   plot.data <- plot.data[plot.data$Test.Report.Produced>=date[1] & plot.data$Test.Report.Produced<=date[2],]
+  dates <- data.cleaned[data.cleaned$Test.Report.Produced>=date[1] & data.cleaned$Test.Report.Produced<=date[2],]
   ggplot(plot.data,
     aes(x = Year.Month, y = Turnover.Time)) +
   labs(title = test, subtitle = "Outliers, defined as ±1.5*IQR, output have not been plotted. Boxplots \nrepresent Median, Q1-Q3 and 1.5*Q1Q3. Red line is the mean.",
@@ -90,14 +91,14 @@ TurnoverPlot <- function(test, date) {
     outlier.shape = NA, outlier.size = .5, notch = FALSE,
     fill = "lightskyblue2", color = "black") +
   stat_summary(aes(y = Turnover.Time, group = 1), fun.y = mean,
-    colour="red", geom="line", group = 1) +
+    colour="red", geom="line", linetype = "dashed", group = 1) +
     theme_minimal() + theme(axis.text.x = element_text(angle = 45, hjust = 1),
       panel.grid.major.x = element_blank(), 
       panel.grid.major.y = element_line(size = .1, color = "black")) +
-    scale_x_discrete(limits = sort(unique(data.cleaned$Year.Month)),
-    breaks = sort(unique(data.cleaned$Year.Month))) +
-    ylim(-5, (quantile(data.cleaned$Turnover.Time)[[4]] + 10)) +
-    geom_hline(yintercept = data.cleaned$Target.Turnaround[1], linetype = "dashed",
+    scale_x_discrete(limits = sort(unique(dates$Year.Month)),
+    breaks = sort(unique(dates$Year.Month))) +
+    ylim(-5, (quantile(plot.data$Turnover.Time)[[4]] + 10)) +
+    geom_hline(yintercept = plot.data$Target.Turnaround[1], linetype = "solid",
     color = "red", size = .5)
 }
 
@@ -108,33 +109,96 @@ TestsPerYear <- function(test, date) {
   Investigation <- Investigation[Investigation$Test.Report.Produced>=date[1] & Investigation$Test.Report.Produced<=date[2],]
   Investigation <- Investigation[c("Year.Reported", "Month.Reported")]
   Investigation <- as.data.frame(table(Investigation))
+  Investigation$Year.Month <- paste(Investigation$Year.Reported, ".", Investigation$Month.Reported, sep = "")
   graph <- ggplot(Investigation[which(Investigation$Freq>0),],  # remove freq 0 values
-             aes(Month.Reported, Freq, group = Year.Reported, color = Year.Reported)) +
-           labs(title = test, y = "Frequency", x = "Month") + geom_point() +
-           geom_line() + theme_minimal() +
-           theme(axis.text.x = element_text(),
-             panel.grid.major.x = element_blank(),
-             panel.grid.major.y = element_line(size = .1, color = "black"))
+    aes(x = Year.Month, y = Freq, group = Year.Reported, color = Year.Reported)) +
+    labs(title = test, y = "Frequency", x = "Year.Month") + geom_point() +
+    geom_line() + theme_minimal() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1),
+      panel.grid.major.x = element_blank(),
+      panel.grid.major.y = element_line(size = .1, color = "black"))
   print(graph)
 }
 
 
-# Define UI for slider app
+##### Plot number of billing types per month, year on year #####
 
 
-ui <- shinyUI(bootstrapPage(
+BillingType <- function(bill, date) {
+  workFreq <- data.cleaned[c("Billing.Category", "Test.Report.Produced", "Year.Reported", "Month.Reported")]
+  workFreq <- workFreq[workFreq$Test.Report.Produced >= date[1] & workFreq$Test.Report.Produced <= date[2],]
+  workFreq <- workFreq[c("Billing.Category", "Year.Reported", "Month.Reported")]
+  workFreq <- as.data.frame(table(workFreq))
+  workFreq <- workFreq[workFreq$Billing.Category == bill,]
+  workFreq$Year.Month <- paste(workFreq$Year.Reported, ".", workFreq$Month.Reported, sep = "")
+  graph <- ggplot(workFreq[which(workFreq$Freq>0),],
+    aes(x = Year.Month, y = Freq, group = Year.Reported, color = Year.Reported)) +
+    labs(title = bill, y = "Frequency (All Tests)", x = "Year.Month") +
+    geom_point() + geom_line() + theme_minimal() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1),
+      panel.grid.major.x = element_blank(),
+      panel.grid.major.y = element_line(size = .1, color = "black"))
+  print(graph)
+}
+
+
+##### Plot the trends in the tests ordered by the top 10 referrer hospitals #####
+
+
+TopReferrers <- function(referrer, date) {
+  topHospitals <<- data.cleaned[c("Hospital")]
+  topHospitals <<- as.data.frame(table(topHospitals))
+  topHospitals <<- topHospitals[ave(-topHospitals$Freq, FUN = rank) <= 10,]
+  referrerHospital <- data.cleaned[c("Hospital", "Test.Report.Produced", "Year.Reported", "Month.Reported")]
+  referrerHospital <- referrerHospital[referrerHospital$Test.Report.Produced >= 
+    date[1] & referrerHospital$Test.Report.Produced <= date[2],]
+  referrerHospital <- referrerHospital[c("Hospital", "Year.Reported", "Month.Reported")]
+  referrerHospital <- as.data.frame(table(referrerHospital))
+  referrerHospital <- subset(referrerHospital, Hospital %in% topHospitals$topHospitals)
+  referrerHospital <- referrerHospital[referrerHospital$Hospital == referrer,]
+  referrerHospital$Year.Month <- paste(referrerHospital$Year.Reported, ".", referrerHospital$Month.Reported, sep = "")
+  graph <- ggplot(referrerHospital[which(referrerHospital$Freq>0),],
+    aes(x = Year.Month, y = Freq, group = Year.Reported, color = Year.Reported)) +
+    labs(title = referrer, y = "Frequency (All Tests)", x = "Year.Month") +
+    geom_point() + geom_line() + theme_minimal() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1),
+      panel.grid.major.x = element_blank(),
+      panel.grid.major.y = element_line(size = .1, color = "black"))
+  print(graph)
+}
+
+
+# Define UI for the shiny app
+
+
+ui <- basicPage(
   titlePanel("trendtracker"),
   sidebarLayout(
     sidebarPanel(
       
       # Allows you to select a test type
       
-      selectInput("turnover.time", "Test Name:",
-                  sort(unique(data.cleaned$Investigation))),
+      pickerInput("turnover.time", "Test Name:",
+                  choices = as.character(sort(unique(data.cleaned$Investigation))),
+                  options = list(`actions-box` = TRUE), 
+                  multiple = T,
+                  selected = as.character(sort(unique(data.cleaned$Investigation)))[1]),
+      
+      pickerInput("bill.type", "Work Type:",
+                  choices = as.character(sort(unique(data.cleaned$Billing.Category))),
+                  options = list(`actions-box` = TRUE), 
+                  multiple = T,
+                  selected = as.character(sort(unique(data.cleaned$Billing.Category)))[1]),
+      
+      pickerInput("referrer.name", "Referrer Name:",
+                  choices = as.character(sort(unique(topHospitals$topHospitals))),
+                  options = list(`actions-box` = TRUE), 
+                  multiple = T,
+                  selected = as.character(sort(unique(topHospitals$topHospitals)))[1]),
       
       # Allows you to select a range of dates
       
-      dateRangeInput("dateRange", "Test report date range:",
+      dateRangeInput("date.range", "Test Report Date Range:",
                      start = sort(unique(data.cleaned$Test.Report.Produced))[1],
                      end = sort(unique(data.cleaned$Test.Report.Produced))[length(sort(unique(data.cleaned$Test.Report.Produced)))])
       ),
@@ -142,10 +206,10 @@ ui <- shinyUI(bootstrapPage(
       # Main panel for plotting the graphs
       
       mainPanel(
-        verbatimTextOutput("dateRangeText"),
         plotOutput(outputId = "turnoverPlot"),
-        plotOutput(outputId = "testsPerYear"))
-    )
+        plotOutput(outputId = "testsPerYear"),
+        plotOutput(outputId = "billingType"),
+        plotOutput(outputId = "topReferrers"))
   )
 )
 
@@ -153,25 +217,26 @@ ui <- shinyUI(bootstrapPage(
 # Define server logic for slider examples
 
 server <- function(input, output) {
-
-  # Pass slider values to the display
   
-  output$values <- renderTable({
-    sliderValues()
+  # Drop down box inputs
+  
+  observe({
+    print(input$turnover.time)
   })
   
-  # Information for the plots
+  # Information on the dates selected
   
-  output$dateRangeText  <- renderText({
-    paste(#as.character(input$dateRange), collapse = " to ",
-    as.character(input$dateRange[1]),
-    as.character(input$dateRange[2])
+  output$date.rangeText  <- renderText({
+    paste(collapse = " to ",
+    as.character(input$date.range[1]),
+    as.character(input$date.range[2])
     )
   })
   
-  output$turnoverPlot <- renderPlot({(TurnoverPlot(input$turnover.time, input$dateRange))})
-  output$testsPerYear <- renderPlot({(TestsPerYear(input$turnover.time, input$dateRange))})
-  
+  output$turnoverPlot <- renderPlot({(TurnoverPlot(input$turnover.time, input$date.range))})
+  output$testsPerYear <- renderPlot({(TestsPerYear(input$turnover.time, input$date.range))})
+  output$billingType <- renderPlot({(BillingType(input$bill.type, input$date.range))})
+  output$topReferrers <- renderPlot({(TopReferrers(input$referrer.name, input$date.range))})
 }
 
 shinyApp(ui = ui, server = server)
